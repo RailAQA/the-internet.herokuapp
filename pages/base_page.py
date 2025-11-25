b92f0baccd189844361288d19aed9d3ce4244d61
rgn2836 <bublik0706@gmail.com>
2025-11-25 21:00:47 +0300

    Написаны основные методы в BagePage

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,4 +1,7 @@
-from locators.home_page_locators import HomePageLocators
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import (NoSuchElementException, StaleElementReferenceException, ElementNotInteractableException, TimeoutException)
+
 
 class BasePage:
     def __init__(self, driver):
@@ -8,14 +11,68 @@ class BasePage:
         """
         Открытие страницы {url}
         """
-        try:
-            self.driver.get(url)
-        except:
-            pass
-
+        return self.driver.get(url)
+        
     def find(self, args):
+        """
+        Поиск элемента по локатору {args}
+        """
         self.driver.find_element(*args)
 
     def click_to(self, args):
-        self.driver.find_element(*args).click()
+        """
+        Клик по элементу с локатором {*args}
+        """
+        try:
+            self.click_to(args)
+            self.driver.find_element(*args).click()
+        except NoSuchElementException:
+            raise AssertionError(f'элемент с локатором {args} не найден')
+        
+    def scroll_to(self, args):
+        self.driver.execute_script("return arguments[0].scrollIntoView({behavior: 'auto', block: 'center', inline: 'center'});", args)
+
+    def wait_element_will_visible(self, timeout: int, args):
+        """
+        Явное ожидание пока элемент с локатором {args} станет видимым в течение {timeout}
+        """
+        try:
+            return WebDriverWait(self.driver, timeout).until(EC.visibility_of_element_located(args))
+        except TimeoutError:
+            raise AssertionError(f'элемент с локатором {args} не появился в течение {timeout}')
+        except TimeoutException:
+            raise AssertionError(f'элемент с локатором {args} не появился в течение {timeout}')
+        
+    def wait_element_will_invisible(self, timeout: int, args):
+        """
+        Явное ожидание пока элемент с локатором {args} станет невидимым в течение {timeout}
+        """
+        try:
+            return WebDriverWait(self.driver, timeout).until(EC.invisibility_of_element_located(args))
+        except TimeoutError:
+            raise AssertionError(f'элемент с локатором {args} не исчез в течение {timeout}')
+        except TimeoutException:
+            raise AssertionError(f'элемент с локатором {args} не исчез в течение {timeout}')
+        
+    def wait_element_will_clickable(self, timeout: int, args):
+        """
+        Явное ожидание пока элемент с локатором {args} станет кликабельным в течение {timeout}
+        """
+        try:
+            return WebDriverWait(self.driver, timeout).until(EC.element_to_be_clickable(args))
+        except TimeoutError:
+            raise AssertionError(f'элемент с локатором {args} не стал кликабельным в течение {timeout}')
+        except TimeoutException:
+            raise AssertionError(f'элемент с локатором {args} не стал кликабельным в течение {timeout}')
+        
+    def wait_element_will_not_clickable(self, timeout: int, args):
+        """
+        Явное ожидание пока элемент с локатором {args} станет НЕкликабельным в течение {timeout}
+        """
+        try:
+            return WebDriverWait(self.driver, timeout).until_not(EC.element_to_be_clickable(args))
+        except TimeoutError:
+            raise AssertionError(f'элемент с локатором {args} не стал HEкликабельным в течение {timeout}')
+        except TimeoutException:
+            raise AssertionError(f'элемент с локатором {args} не стал HEкликабельным в течение {timeout}')
 
