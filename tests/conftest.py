8eb57d751c97bb845f3deeb1916590babafa21a7
rgn2836 <bublik0706@gmail.com>
2025-11-26 18:55:38 +0300

    Прописан хук на скриншот для аллюр отчета

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 import pytest
+import allure
 
 @pytest.fixture
 def driver():
@@ -7,3 +8,18 @@ def driver():
     driver.maximize_window()
     yield driver
     driver.close()
+
+@pytest.hookimpl(hookwrapper=True)
+def pytest_runtest_makereport(item, call):
+    outcome = yield
+    report = outcome.get_result()
+    
+    if report.failed:
+        driver = item.funcargs.get('driver')
+        if driver and hasattr(driver, 'get_screenshot_as_png'):
+            # Скриншот
+            allure.attach(
+                driver.get_screenshot_as_png(),
+                name=f"screenshot_{item.name}_{report.when}",
+                attachment_type=allure.attachment_type.PNG
+            )
